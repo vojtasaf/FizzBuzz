71f3422157a7e4efbcc5b55908078140e699edd5
Vojta Safranek <vojtech.safranek@gmail.com>
2021-04-05 12:32:05 +0200

    Different approach

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,11 @@
 for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+    fizzOrBuzz = False
+    if i % 3 == 0:
+        print("Fizz", end="")
+        fizzOrBuzz = True
+    if i % 5 == 0:
+        print("Buzz", end="")
+        fizzOrBuzz = True
+    if (not fizzOrBuzz):
+        print(i, end="")
+    print()
